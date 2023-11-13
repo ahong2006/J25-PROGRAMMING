57e02100f10eb4d96d87cea31c868b8205cc66c9
ISAGI <124577520+ahong2006@users.noreply.github.com>
2023-11-13 08:55:34 +0100

    Create MorseName1.0.ino

---
+++ Arduino/MorseCode/MorseName1.0/MorseName1.0.ino
@@ -0,0 +1,58 @@
+int dotDuration = 100;
+int dashDuration = 3 * dotDuration;
+int waitDuration = 3 * dotDuration;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // A
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(dotDuration);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(dotDuration);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(dotDuration * 3);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(waitDuration);  // 
+
+  // N
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(dashDuration);
+  digitalWrite( );
+  delay(waitDuration);
+
+  // G
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(dashDuration);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(dashDuration);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(dashDuration);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(waitDuration);
+
+  // E
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(dotDuration);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(waitDuration);
+
+  // L
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(dotDuration);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(dashDuration);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(dotDuration);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(dotDuration);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(dotDuration);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(waitDuration);  // 
+}
+
+
+ 
