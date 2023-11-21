933544a665d3e763cea8efc213df839b194d8e7f
ISAGI <124577520+ahong2006@users.noreply.github.com>
2023-11-21 11:39:14 +0100

    Create PROBLEMSOLVER2.ino

---
+++ Arduino/PROBLEMSOLVER/PROBLEMSOLVER2/PROBLEMSOLVER2.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // put your setup code here, to run once:
+for(int numeroProblema=1;numeroProblema<=30;numeroProblema++){
+  for(int n%2==1 && n%3==0){
+  if(n! = 17){
+  resolverProblemaDibujo(numeroProblema);
+
+}
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+void resolverProblemaDibujo(int n){
+  Serial.print("Resolviendo Problema");
+  Serial.println(n);
+}
