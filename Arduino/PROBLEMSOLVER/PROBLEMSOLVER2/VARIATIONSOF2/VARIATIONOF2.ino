3b1397857374b399eefc3e1844f6846d2688c1bc
ISAGI <124577520+ahong2006@users.noreply.github.com>
2023-11-21 11:40:30 +0100

    Create VARIATIONOF2.ino

---
+++ Arduino/PROBLEMSOLVER/PROBLEMSOLVER2/VARIATIONSOF2/VARIATIONOF2.ino
@@ -0,0 +1,37 @@
+void setup() {
+  // put your setup code here, to run once:
+for(int numeroProblema=1;numeroProblema<=30;numeroProblema=numeroProblema+2){
+  resolverProblemaDibujo(^P);// solo problema
+  //cambio de orden
+for(int n= 30; n>=1 ; n--){
+  resolverProblemaDibujo(n);
+//todosmenosel17
+for(int n= 30 ; n>=1; n++){
+  if(n! = 17){
+  resolverProblemaDibujo(n);
+  }
+}
+}
+}
+}
+for(__________){
+  if(n==17){n++;}
+  resolverProblemaDibujo(n);
+//todosmenos17y23
+}
+for(__________){
+  if(n!=17&&n!=23){
+    resolverProblemaDibujo(n);
+  }
+}
+if(!(n==13|||n==23)){
+
+}
+//resolver problemas impares multiples de 3
+for(int n%2==1 && n%3==0){
+  if(n! = 17){
+  resolverProblemaDibujo(n);
+  }
+}
+void loop() {
+  // put your main code here, to run repeatedly:
