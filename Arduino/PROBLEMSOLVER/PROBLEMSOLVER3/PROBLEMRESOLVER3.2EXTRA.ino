aba87dbda0c83e81729951e55a3ece4d8f8e6c4a
ISAGI <124577520+ahong2006@users.noreply.github.com>
2023-11-23 09:06:13 +0100

    Update PROBLEMRESOLVER3.2EXTRA.ino

--- Arduino/PROBLEMSOLVER/PROBLEMSOLVER3/PROBLEMRESOLVER3.2EXTRA.ino
+++ Arduino/PROBLEMSOLVER/PROBLEMSOLVER3/PROBLEMRESOLVER3.2EXTRA.ino
@@ -1,1 +1,21 @@
+void setup() {
+  for (int numeroProblema = 1; numeroProblema <= 100; numeroProblema++) {
+    if(SumaPar(numeroProblema)) {/
+      resolverProblemaDibujo(numeroProblema);
+    }
+  }
+}
+bool SumaPar(int numeroProblema){
+if(numeroProblema=100){return;false}
+int unidades = num % 10;
+int decenas = num / 10) % 10;
+int Suma = unidades + decenas ;
+if( Suma %2==0){ return suma%2 == 0;} 
+void loop() {
+  // put your main code here, to run repeatedly:
+}
 
+void resolverProblemaDibujo(int n) {
+  Serial.print("Resolviendo Problema ");
+  Serial.println(n);
+}
