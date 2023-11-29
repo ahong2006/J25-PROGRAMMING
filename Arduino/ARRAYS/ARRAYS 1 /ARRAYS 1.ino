42bba8f042639dcafcd880199538d68a2e2f9bb8
ISAGI <124577520+ahong2006@users.noreply.github.com>
2023-11-29 22:21:18 +0100

    Create ARRAYS 1.ino

---
+++ Arduino/ARRAYS/ARRAYS 1 /ARRAYS 1.ino
@@ -0,0 +1,26 @@
+void setup() {
+  // put your setup code here, to run once:
+  serial.begin(9600)
+  String word = [fate]
+  Serial.println(fate); // writes the word
+  Serial.println(fate [0]);//writes the first letter
+  fate
+  String.juanito = fate [1]// second letter
+  Serial.println(juanito)
+  fate[1]= fate [0];
+  fate[0]= juanito
+  Serial.println(fate);// actually, it just chage ther order of the letter, for an ex: fate becomes to afte
+
+  //String functionX(String W){
+  // String T = W
+  //W[1]= W[0];
+  // W[0] = T;
+  // return W;
+  //}
+}
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
