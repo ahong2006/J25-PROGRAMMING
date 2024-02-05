75ae93a6dccf11575962454ad2f7b096f527bb5f
ISAGI <124577520+ahong2006@users.noreply.github.com>
2024-02-05 09:54:35 +0100

    Create EJERCICIO2.java

---
+++ JAVA/EJERCICIOS 5FEB/EJERCICIOS/EJERCICIO2/EJERCICIO2.java
@@ -0,0 +1,9 @@
+public class ArithmeticTest {
+   public static void main(String[] args) {
+      int number1 = 98;
+      int number2 = 5;
+      int sum, difference, product, quotient, remainder;
+      sum = (31 * number1) + (17 * number2);
+      System.out.println ("The sum of 31 times " + number1 + " and 17 times " + number 2 + " is " + sum)
+  }
+}
