34f8b5ccf1bedb25a259a1f7bd82ec03a292cc39
ISAGI <124577520+ahong2006@users.noreply.github.com>
2024-02-05 09:53:44 +0100

    Update EJERCICIO1.java

--- JAVA/EJERCICIOS 5FEB/EJERCICIOS/EJERCICIO1 /EJERCICIO1.java
+++ JAVA/EJERCICIOS 5FEB/EJERCICIOS/EJERCICIO1 /EJERCICIO1.java
@@ -1,1 +1,19 @@
-
+public class ArithmeticTest {     
+   public static void main(String[] args) {
+    int number1 = 98;
+     int number2 = 5;
+    int sum, difference, product, quotient, remainder;  
+    sum = number1 + number2;
+      difference = number1 - number2;
+      product = number1 * number2;
+     quotient = number1 / number2;
+      remainder = number1 % number2;
+      System.out.println("The sum, difference, product, quotient and remainder of " + number1 + "and" + sum + "," + difference + "," + product + "," + quotient + ", and" + remainder);
+      ++number1;
+    --number2;
+      System.out.println("number1 after increment is " + number1);  
+      System.out.println("number2 after decrement is " + number2);
+        quotient = number1 / number2; 
+      System.out.println("The new quotient of " + number1 + " and " + number2 + " is " + quotient);
+   }
+}
