c8130b240e21fbfe92b43d7763613f6b4852ecc5
ISAGI <124577520+ahong2006@users.noreply.github.com>
2024-02-05 09:55:10 +0100

    Create EJERCICIO3.java

---
+++ JAVA/EJERCICIOS 5FEB/EJERCICIOS/EJERCICIO3/EJERCICIO3.java
@@ -0,0 +1,22 @@
+public class ArithmeticTest {
+   public static void main(String[] args) {
+      int number1 = 98;
+      int number2 = 5;
+      int number3 = 77;
+      int sum, product;
+   
+      sum = number1 + number2 + number3;
+      product = number1 * number2 * number3;
+  
+      System.out.print("The sum and the product of ");
+      System.out.print(number1);
+      System.out.print(" , ");
+      System.out.print(number2);
+      System.out.print(" and ");
+      System.out.print(number3);
+      System.out.print(" are ");
+      System.out.print(sum);
+      System.out.print(" and ");
+      System.out.print(product);
+   }
+}
