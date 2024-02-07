0a14e792399c4fdb7ada59d19593ee0a122b0b91
ISAGI <124577520+ahong2006@users.noreply.github.com>
2024-02-07 23:02:15 +0100

    Update PROBLEMSOLVER5.ino

--- Arduino/PROBLEMSOLVER/PROBLEMSOLVER5/PROBLEMSOLVER5.ino
+++ Arduino/PROBLEMSOLVER/PROBLEMSOLVER5/PROBLEMSOLVER5.ino
@@ -1,18 +1,18 @@
-void setup(){ 
- Serial.begin(9600);
+void setup() {
+  Serial.begin(9600);
 
- for(int n=1; n<=30; n++){
-  if(n%2==1 && n%3==0){
-    resolverProblemaDibujo(n);
+  for (int n = 1; n <= 100; n++) {
+    if (n % 2 == 1 || n % 6 == 0) {
+      resolverProblemaDibujo(n);
+    }
   }
 }
+
+void loop() {
+  // Nothing to loop
 }
 
-void loop(){   
-} 
-
-void resolverProblemaDibujo(int n){ 
- Serial.print("Resolviendo problema"); 
- Serial.println(n); 
-
-} 
+void resolverProblemaDibujo(int n) {
+  Serial.print("Resolviendo problema ");
+  Serial.println(n);
+}
