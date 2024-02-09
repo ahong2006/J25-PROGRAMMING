1508cdec978ddceb9527de0cf713a37c229cb948
ISAGI <124577520+ahong2006@users.noreply.github.com>
2024-02-09 08:14:27 +0100

    Update Codigo Principal.ino

--- Arduino/Proyecto Arduino/CODIGO PRINCIPAL/Codigo Principal.ino
+++ Arduino/Proyecto Arduino/CODIGO PRINCIPAL/Codigo Principal.ino
@@ -1,1 +1,31 @@
+#include "morse.h"
+#include "cook.h"
+#include "song.h"
 
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial. available() > 0) {
+    String command = Serial.readStringUntil('\n');
+
+    Serial.println("You have written: " + command);
+
+    if (command.startsWith("m: ") || command.startsWith("morse ")) {
+      String message = command.substring(command.indexOf(' ') + 1);
+      translateToMorse(message);
+    }
+
+    if (command.equalsIgnoreCase("recipe")) {
+      cook();
+    }
+
+    if (command.equalsIgnoreCase("play")) {
+      playSong();
+    } 
+    else if (command.equalsIgnoreCase("stop")) {
+      stopSong();
+    }
+  }
+}
