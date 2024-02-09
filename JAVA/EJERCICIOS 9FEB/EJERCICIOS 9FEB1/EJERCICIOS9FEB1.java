6a3de70106f1f463eaa31d4f6e32548d358350ad
ISAGI <124577520+ahong2006@users.noreply.github.com>
2024-02-09 19:43:54 +0100

    Update EJERCICIOS9FEB1.java

--- JAVA/EJERCICIOS 9FEB/EJERCICIOS 9FEB1/EJERCICIOS9FEB1.java
+++ JAVA/EJERCICIOS 9FEB/EJERCICIOS 9FEB1/EJERCICIOS9FEB1.java
@@ -1,1 +1,12 @@
+public class ThreeFiveSevenSum {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 1000; i++) {
+            if ((i % 3 == 0 || i % 5 == 0 || i % 7 == 0) && !(i % 15 == 0 || i % 21 == 0 || i % 35 == 0 || i % 105 == 0)) {
+                sum += i;
+            }
+        }
+        System.out.println("Sum of numbers divisible by 3, 5, or 7 but not by 15, 21, 35, or 105: " + sum);
+    }
+}
 
