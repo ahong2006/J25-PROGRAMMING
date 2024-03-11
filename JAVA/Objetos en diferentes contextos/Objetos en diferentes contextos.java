16697b2e430e1661cb79aeaabafcfc4cb312dddc
ISAGI <124577520+ahong2006@users.noreply.github.com>
2024-03-11 08:50:00 +0100

    Create Objetos en diferentes contextos.java

---
+++ JAVA/Objetos en diferentes contextos/Objetos en diferentes contextos.java
@@ -0,0 +1,43 @@
+public class AnimeFigure {
+
+  private String series;        // Name of the anime/manga LIKE: Doraemon, Jujustsukaisen
+  private String character;     // Name of the character , like: Satoru, Goku, Sakurajima Mai
+  private String manufacturer;  // Company that produced the figure Like: Bandai, Good Smile , taito 
+  private double scale;         // Scale of the figure compared to real life like: 1/8, 1/7, 1/1
+  private String paintwork;     // Description of the paint application 
+  private String material;      // Material the figure is made from ,like: PVC, ABS
+  private String releaseDate;   // Date the figure was released
+  private String accessories;   // Any additional items included with the figure , like :extra faces, weapons
+}
+
+
+public class Gunpla {
+
+
+  private String name;  // EXIA, STRIKE, UNICORN
+  private String grade;// HG, RG, MG , PG
+  private double scale; // 1/144, 1/100, 1/60
+  private String manufacturer;  // BANDAI, DABAN, MOMOKODRAGON
+  private int numRunners;
+  private int partsCount;
+  private String injectionColor;  // red, white yellow
+  private String releaseDate;    
+
+}
+
+
+
+public class MangaBook {
+
+  private String title;           // Title of the manga series
+  private String author;          // Author(s) of the manga
+  private int pageNumber;      
+  private String publicationDate; 
+  private String publisher;       // Publishing company
+  private String genre;         
+  private String magazine;      
+  private String isbn;            // International Standard Book Number
+}
+
+
+  
